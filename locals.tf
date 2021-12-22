be9d3c2837b8e4ec606940efb5840e37b9939408
Savita Pal <savitap@ad.corp.google.com>
2021-12-22 14:35:59 +0530

    Committed Changes

--- locals.tf
+++ locals.tf
@@ -8,11 +8,18 @@ locals {
   )
 
   # Specific prefix for service’s resources
+  /*
   dp_prefix  = replace(replace(local.name_prefix, "[SERVICE]", "dp"), "-", "_") 
   bq_prefix  = replace(replace(local.name_prefix, "[SERVICE]", "bq"), "-", "_")
   kms_prefix  = replace(replace(local.name_prefix, "[SERVICE]", "kms"), "-", "_") 
   gcs_prefix  = replace(replace(local.name_prefix, "[SERVICE]", "gcs"), "-", "_")
   log_prefix  = replace(replace(local.name_prefix, "[SERVICE]", "log"), "-", "_")
+  */
+  dp_prefix   = replace(local.name_prefix, "[SERVICE]", "dp") 
+  bq_prefix   = replace(local.name_prefix, "[SERVICE]", "bq")
+  kms_prefix  = replace(local.name_prefix, "[SERVICE]", "kms")
+  gcs_prefix  = replace(local.name_prefix, "[SERVICE]", "gcs")
+  log_prefix  = replace(local.name_prefix, "[SERVICE]", "log")
 }
 
 data "google_project" "project" {
