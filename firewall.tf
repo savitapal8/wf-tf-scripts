689314fe4c911177d2f59419224da9922bc50d04
Savita Pal <savitap@ad.corp.google.com>
2021-12-22 19:06:48 +0530

    Added resources

--- firewall.tf
+++ firewall.tf
@@ -3,7 +3,7 @@ resource "google_compute_firewall" "fw-iap" {
   name          = "${local.fw_prefix}_fw1"
   provider      = google-beta
   direction     = "INGRESS"
-  network       = google_compute_network.ilb_network.id
+  network       = google_compute_network.vpc_network_1.id
   source_ranges = ["130.211.0.0/22", "35.191.0.0/16", "35.235.240.0/20"]
   allow {
     protocol = "tcp"
@@ -15,7 +15,7 @@ resource "google_compute_firewall" "fw-ilb-to-backends" {
   name          = "${local.fw_prefix}_fw2"
   provider      = google-beta
   direction     = "INGRESS"
-  network       = google_compute_network.ilb_network.id
+  network       = google_compute_network.vpc_network_1.id
   source_ranges = ["10.0.0.0/24"]
   target_tags   = ["http-server"]
   allow {
