cbf172236699efe727cbe49241e25167ab1ace37
Savita Pal <savita.pal08@gmail.com>
2022-06-15 14:48:19 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,8 +2,10 @@ provider "google" {
   user_project_override = true
   access_token          = var.access_token
   project               = var.project_id
+  version               = 4.8.0
 }
 
 provider "google-beta"{
   access_token          = var.access_token
+  version               = 4.8.0
 }
